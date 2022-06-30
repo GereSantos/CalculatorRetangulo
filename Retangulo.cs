4472da8c1408e921f185e34dda0d00a924c96206
Geremias Santos <Geremias99999@gmail.com>
2022-06-30 14:04:45 -0300

    CalculatorRetangulo

---
+++ Retangulo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorRetangulo
+{
+    public class Retangulo
+    {
+        public double Largura { get; set; }
+        public double Altura { get; set; }
+
+        public double Area()
+        {
+            return Altura * Largura;
+        }
+
+        public double Perimetro()
+        {
+            return 2 * (Altura + Largura);
+        }
+
+        public double Diagonal()
+        {
+            return (Altura * Altura) + (Largura * Largura);
+        }
+
+
+    }
+}
